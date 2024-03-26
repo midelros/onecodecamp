908ccb120d917b23ad089ed08499ea368f608bcb
midelros <mdrdesign.ps@gmail.com>
2024-03-25 21:58:46 -0400

    Day 5 codes saved

---
+++ DAY 5 - Intermediate CSS/Intro to Javascript/index.js
@@ -0,0 +1,9 @@
+let firstName = "John";
+
+console.log("Hello!",firstName)
+
+// 1. Should be meaningful and descriptive.
+// 2. Should not start with a number.
+// 3. Should not contain a space or hyphen.
+// 4. Case-sensitive
+// 5. Cannot use reserved keywords
